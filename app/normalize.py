a18fe282c7537c3d833ae23257e660b700925a19
s0r3 <ggoodson2432@gmail.com>
2022-11-03 14:04:59 -0400

    Update normalize.py

--- app/normalize.py
+++ app/normalize.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse, urlunparse, parse_qs
+import os
+import logging as log
 
 
 def normalizeUrl(url) -> str:
@@ -10,6 +12,26 @@ def normalizeUrl(url) -> str:
 
     return urlunparse(url_parts)
 
+def normalizeLocal(url) -> str:
+    local_path = "/mnt/d/homeword/"
+    local_url = local_path + url
+    log.debug(f"local_url: {local_url}")
+    # Generates a directory listing of the local path
+    log.debug(f"Normalize:Local: Generating directory tree of {local_path}")
+
+    local_path_tree = []
+    with open("./cache/rootmap.txt", 'w') as file:
+        for root, dirs, files in os.walk(local_path):
+            for name in files:
+                file.write(os.path.join(root, name) + "\n")
+                local_path_tree.append(os.path.join(root, name))
+    log.debug(f"Normalizer:Local:Tree: Generated")
+    # Checks if the url_parts.path is in the directory listing
+    if local_url in local_path_tree:
+        # If it is, it returns the fill path
+        return local_url
+    else:
+        return 404
 
 def normalizeYT(url_parts) -> str:
     # remove the list query param as it causes excess delay in loading information
